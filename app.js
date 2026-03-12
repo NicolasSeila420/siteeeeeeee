75945d8184a10f5cc116d7aaa8cb5768e6e90e87
NicolasSeila420 <peres.nicolas@escola.pr.gov.br>
2026-03-12 12:34:07 -0300

    fgf

--- app.js
+++ app.js
@@ -72,9 +72,21 @@ document.addEventListener('keydown', function (event) {
 document.querySelector("#files").addEventListener("change",(e) => {
   if(window.File && window.FileReader && window.FileList && window.Blob){
     const files = e.target.files;
-    console.log(files);
+    const output = document.querySelector('#result');
+
+    for(let i = 0;i < files.length;i++){
+      if(!files[i].type.match("image")) continue;
+      const picReader = new FileReader();
+      picReader.addEventListener("load", function(event){
+        const picFIle = event.target;
+        const div = document.createElement("div");
+        div.innerHTML = `<img class="thumbnail" src="${picFIle.result}" title="${picFIle.name}"/>`;
+        output.appendChild(div);
+      })
+        picReader.readAsDataURL(files[i]);
+    }
   } else {
     alert("Your browser is Shit")
   }
 })
-    
+ 
